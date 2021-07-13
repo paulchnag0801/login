b4edf5ff7eab6b254ff4e68894a1ba4fa60cb0a9
paulchnag0801 <x1232178987@gmail.com>
2021-07-13 23:48:09 +0800

    feat: add mongoose

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+mongoose.connect('mongodb://localhost/todo-list', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+app.get('/', (req, res) => {
+  res.send('hello world') 
+})
+
+
+
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
+})
