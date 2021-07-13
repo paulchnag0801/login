ba69e5bc11c2b3e0d00332850a0e45ec7a6a1f48
paulchnag0801 <x1232178987@gmail.com>
2021-07-14 00:13:24 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 const app = express()
 
 mongoose.connect('mongodb://localhost/login-pwd-practice', {
@@ -18,11 +19,12 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('hello world') 
+  res.render('login')
 })
-
-
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
