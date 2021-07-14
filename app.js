b9c8d481d4ac500d9cdc6e8d5dd0280aa4922a66
paulchnag0801 <x1232178987@gmail.com>
2021-07-14 11:48:25 +0800

    feat : "change app js  "

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const app = express()
 
-mongoose.connect('mongodb://localhost/login-pwd-practice', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+const routes = require('./routes')
 
+require('./config/mongoose')
 // 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(routes)
 
 app.get('/', (req, res) => {
   res.render('login')
