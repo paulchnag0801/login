b370ce4cf32da81e20e95aad040304882f9e9791
paulchnag0801 <x1232178987@gmail.com>
2021-07-14 11:37:04 +0800

    feat : "change users js  detail"

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,22 +3,38 @@ const router = express.Router()
 
 const Account = require('../../models/accounts.js')
 
+router.get('/', (req, res) => {
+  return res.redirect('/login')
+})
+
 router.get('/login', (req, res) => {
-  res.render('login')
+  return res.render('login')
 })
 
 router.post('/login', (req, res) => {
-  res.render('login')
+  return Account.findOne({ email: req.body.email })
+    .lean()
+    .then((user) => {
+      if (!user) {
+        const alert = '該 email 尚未註冊'
+        return res.render('login', { alert })
+      }
+      if (user.password !== req.body.password) {
+        const alert = '您輸入的密碼有誤'
+        return res.render('login', { alert })
+      }
+      return res.redirect(`/welcome/${user._id}`)
+    })
 })
 
 router.get('/register', (req, res) => {
-  res.render('register')
+  return res.render('register')
 })
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
   // 檢查使用者是否已經註冊
-  User.findOne({ email }).then((user) => {
+  Account.findOne({ email }).then((user) => {
     // 如果已經註冊：退回原本畫面
     if (user) {
       console.log('User already exists.')
@@ -30,7 +46,7 @@ router.post('/register', (req, res) => {
       })
     } else {
       // 如果還沒註冊：寫入資料庫
-      return User.create({
+      return Account.create({
         name,
         email,
         password,
@@ -39,6 +55,15 @@ router.post('/register', (req, res) => {
         .catch((err) => console.log(err))
     }
   })
+})
+
+router.get('/welcome/:id', (req, res) => {
+  const id = req.params.id
+  return Account.findById(id)
+    .lean()
+    .then((user) => {
+      res.render('welcome', { user })
+    })
 })
 
 module.exports = router
